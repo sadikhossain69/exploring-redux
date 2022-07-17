1200f94ca9de34c22e970abfbec0d05842ed8852
Dev Sadik <sadikhossain433@gmail.com>
2022-07-17 19:44:39 +0600

    multiple reducer #2

--- multipleReducer.js
+++ multipleReducer.js
@@ -1,13 +1,23 @@
-const { createStore } = require("redux")
+const { createStore, combineReducers } = require("redux")
 
 // products constant
 const GET_PRODUCTS = 'GET_PRODUCTS'
 const ADD_PRODUCT = 'ADD_PRODUCT'
 
-// initial state
+// cart constant
+const GET_CART_ITEMS = 'GET_CART_ITEMS'
+const ADD_CART_ITEM = 'ADD_CART_ITEM'
+
+// products initial state
 const initialProductState = {
     products: ['sugar', 'salt'],
     numberOfProducts: 2,
+}
+
+// carts initial state
+const initialCartState = {
+    cart: ['sugar'],
+    numberOfProducts: 1
 }
 
 // products action
@@ -20,6 +30,20 @@ const getProducts = () => {
 const addProducts = (product) => {
     return {
         type: ADD_PRODUCT,
+        payload: product
+    }
+}
+
+// cart actions
+const getCart = () => {
+    return {
+        type: GET_CART_ITEMS
+    }
+}
+
+const addCart = (product) => {
+    return {
+        type: ADD_CART_ITEM,
         payload: product
     }
 }
@@ -37,12 +61,36 @@ const productsReducer = (state = initialProductState, action) => {
                 numberOfProducts: state.numberOfProducts + 1
             };
         default:
-            state;
+            return state;
     }
 }
 
+// cart reducer
+const cartReducer = (state = initialCartState, action) => {
+    switch (action.type) {
+        case GET_CART_ITEMS:
+            return {
+                ...state
+            }
+        case ADD_CART_ITEM:
+            return {
+                cart: [...state.cart, action.payload],
+                numberOfProducts: state.numberOfProducts + 1
+            }
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    productR: productsReducer,
+    cartR: cartReducer,
+})
+
 // store
-const store = createStore(productsReducer)
+// const store = createStore(productsReducer)
+// const store = createStore(cartReducer)
+const store = createStore(rootReducer)
 
 // subscribe 
 store.subscribe(() => {
@@ -52,3 +100,6 @@ store.subscribe(() => {
 // dispatch
 store.dispatch(getProducts())
 store.dispatch(addProducts('pen'))
+
+store.dispatch(getCart())
+store.dispatch(addCart('pen'))
